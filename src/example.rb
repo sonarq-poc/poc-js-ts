ccc76162c34a79de5d6703fd9996288f0949ab0b
Mariana Souza <m.souza@samsung.com>
2023-02-13 15:43:54 -0500

    RB code test

---
+++ src/example.rb
@@ -0,0 +1,22 @@
+class Class
+    alias old_new new
+    def new(*args)
+      print "Creating a new ", self.name, "\n"
+      print "Creating a new ", self.name, "\n"
+      old_new(*args)
+    end
+  end
+  class Class
+    alias old_new new
+    def new(*args)
+      print "Creating a new ", self.name, "\n"
+      print "Creating a new ", self.name, "\n"
+      old_new(*args)
+    end
+  end
+  
+  class Name
+  end
+  
+  n = Name.new
+  # this is a comment
